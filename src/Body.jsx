259da90b0812de33e0ac4dda5ef94620d5807576
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-03-20 18:04:53 +0530

    New Changes

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,10 +3,10 @@ import rotateGif from "./assets/rotate.gif";
 import "./Body.css";
 export default function Body() {
   return (
-    <div className="Body row">
-      <div className="col-lg-5 col-md-8">
-        <h2>$URANUS Web</h2>
-        <p>
+    <div className="Body">
+      <h2>$URANUS Web</h2>
+      <div className="top">
+        <div className="para">
           Uranus, the ruler of Aquarius, starts 2024 in retrograde, going direct
           on <b>Saturday, January 27</b>, then retrogrades again on{" "}
           <b>Sunday, September 1</b>. This suggests ongoing evolution in your
@@ -17,10 +17,15 @@ export default function Body() {
           <br /> In 2024, the location of the Sun on the March equinox is in the
           constellation of Pisces but also on the border of Aquarius. So,{" "}
           <b>we are slowly moving into a new age, from Pisces to Aquarius</b>.
-        </p>
-      </div>
-      <div className="col-lg-5 col-md-8">
-        <img src={rotateGif} alt="" />
+          Aquarius is ruled by Uranus, the planet that governs innovation,
+          technology, and surprising events. Uranus perfectly mirrors Aquarius'
+          distinctive attitude, complementing the nontraditional nature of these
+          visionary air signs.
+        </div>
+
+        <div className="img">
+          <img src={rotateGif} alt="Uranus image" />
+        </div>
       </div>
     </div>
   );
